bc56a300a8af562fdb12aa01555e1ad49a43336d
MishaOnyshchenko <misha.onyshchenko@gmail.com>
2018-05-05 23:02:10 +0300

    updated

--- hw1_27.12.2017/src/Runner.java
+++ hw1_27.12.2017/src/Runner.java
@@ -10,10 +10,10 @@ public class Runner {
 7.Найти в массиве чисел элементы с наибольшим(max) и наименьшим(min) значениями. Вывести эти 2 элемента на экран, а также посчитать и вывести сумму всех остальных элементов (min и max в сумму не входят).
 
 8. Создать строку с произвольным словом или набором букв. Преобразовать в массив. Вывести массив каждую букву(символ).
-
-Результат залить к себе в репозиторий. (edited) (edited)*/
+*/
     public static void main(String[] args) {
-        checkIfEven(223);
+        //checkIfEven(223);
+        closerToTen(-11.5, 12.5);
     }
     /*Создать программу, проверяющую и сообщающую на экран,
     является ли целое число записанное в переменную n, чётным либо нечётным.*/
@@ -27,4 +27,17 @@ public class Runner {
 
     /*Создать программу, выводящую на экран ближайшее к 10 из двух чисел,
     записанных в переменные m и n. Например, среди чисел 8,5 и 11,45 ближайшее к десяти 11,45.*/
+    public static void closerToTen(double m, double n){
+        double result, closeToTenM, closeToTenN;
+        closeToTenM = 10 - m;
+        closeToTenN = 10 - n;
+
+        if(Math.abs(closeToTenM) < Math.abs(closeToTenN)){
+            result = m;
+        }
+        else {
+            result = n;
+        }
+        System.out.println("Closer to 10: " + result);
+    }
 }
