398b9d1dddfca029a29d188dc29be26999edfaf8
MishaOnyshchenko <misha.onyshchenko@gmail.com>
2018-05-05 20:31:49 +0300

    new one

--- hw1_27.12.2017/src/Runner.java
+++ hw1_27.12.2017/src/Runner.java
@@ -2,7 +2,11 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("new");
-        System.out.println("stratocaster");
+        System.out.println("stratocaster new");
+        System.out.println("telek");
+        System.out.println("one more");
+        System.out.println("Time");
+        System.out.println("finished");
 
     }
 }
