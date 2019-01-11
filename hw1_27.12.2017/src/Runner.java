78ac4645132d825c63bd2cb386cdf647b88330b5
MishaOnyshchenko <misha.onyshchenko@gmail.com>
2019-01-12 01:07:28 +0200

    task.txt

--- hw1_27.12.2017/src/Runner.java
+++ hw1_27.12.2017/src/Runner.java
@@ -4,20 +4,20 @@ import java.util.Random;
 public class Runner {
 
     public static void main(String[] args) {
-        checkIfEven(223);
-        closerToTen(-11.5, 12.5);
+        checkIfEven(27);
+        closerToTen(-11.5, 32.5);
         printWithLoops();
-        multiplyToFive();
-        fillArray();
+        multiplesOfFive();
+        fillShortArray();
         sumFromMinToMax();
-        stringToCharArray("The\ngrass\nwas\ngreener");
+        stringToCharArray("\nThe\ngrass\nwas\ngreener");
     }
 
     /*Создать программу, проверяющую и сообщающую на экран,
     является ли целое число записанное в переменную n, чётным либо нечётным.*/
-    public static void checkIfEven(int n){
+    public static void checkIfEven(int n) {
         boolean isEven = false;
-        if(n%2 == 0){
+        if (n % 2 == 0) {
             isEven = true;
         }
         System.out.println("Number " + n + " is even: " + isEven);
@@ -25,103 +25,109 @@ public class Runner {
 
     /*Создать программу, выводящую на экран ближайшее к 10 из двух чисел,
     записанных в переменные m и n. Например, среди чисел 8,5 и 11,45 ближайшее к десяти 11,45.*/
-    public static void closerToTen(double m, double n){
+    public static void closerToTen(double m, double n) {
         double result, closeToTenM, closeToTenN;
-        closeToTenM = 10 - m;
-        closeToTenN = 10 - n;
+        closeToTenM = Math.abs(10 - m);
+        closeToTenN = Math.abs(10 - n);
 
-        if(Math.abs(closeToTenM) < Math.abs(closeToTenN)){
+        if (closeToTenM < closeToTenN) {
             result = m;
-        }
-        else {
+        } else {
             result = n;
         }
-        System.out.println("Closer to 10: " + result);
+        System.out.println("Closer to 10 (" + m + " or " + n + "): " + result);
     }
 
     /*Вывести на экран числа от -10 до 25 с помощью всех известных циклов.*/
-    public static void printWithLoops(){
+    public static void printWithLoops() {
 
-        System.out.println("while loop");
+        System.out.println("While loop (from -10 to 25):");
         int w = -10;
-        while(w < 26){
+
+        while (w <= 25) {
             System.out.println("w = " + w);
             w++;
         }
 
-        System.out.println("\ndo-while loop");
+
+        System.out.println("\nDo-while loop (from -10 to 25):");
         int dw = -10;
-        do{
+        do {
             System.out.println("dw = " + dw);
             dw++;
         }
-        while(dw < 26);
+        while (dw <= 25);
 
-        System.out.println("\nfor loop");
-        for(int i = -10; i < 26; i++){
+
+        System.out.println("\nFor loop (from -10 to 25):");
+        for (int i = -10; i <= 25; i++) {
             System.out.println("i = " + i);
         }
+        System.out.println();
     }
 
     /*Вывести на экран все числа от 1 до 100, которые делятся на 5 без остатка.*/
-    public static void multiplyToFive(){
-        for(int i = 1; i < 101; i++){
-            if(i%5 == 0){
-                System.out.println("i = " + i);
+    public static void multiplesOfFive() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0) {
+                System.out.println(i + " multiple of 5");
             }
         }
     }
 
     /*Заполнить массив short типа числами от 100 до 0.*/
-    public static void fillArray(){
+    public static void fillShortArray() {
         short[] arr = new short[101];
-        for(short i = 0; i < 101; i++){
+        for (short i = 100; i >= 0; i--) {
             arr[i] = i;
         }
-        System.out.println("arr = " + Arrays.toString(arr));
+        System.out.println("\nshort[] arr = " + Arrays.toString(arr));
     }
 
-    /*Найти в массиве чисел элементы с наибольшим(max) и наименьшим(min) значениями.
+    /*Найти в массиве чисел элементы с наибольшим (max) и наименьшим (min) значениями.
     Вывести эти 2 элемента на экран, а также посчитать и вывести сумму всех остальных элементов
     (min и max в сумму не входят).*/
-    public static void sumFromMinToMax(){
-        //create and fill array
+    public static void sumFromMinToMax() {
+        //create and fill an array
         final int SIZE = 10;
-        int[] arr = new int [SIZE];
-        for (int i = 0; i < SIZE; i++){
-            arr[i] = (int)(Math.random() * 50 + 5);
+        int[] randomArray = new int[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            randomArray[i] = (int) (Math.random() * 50 + 5);
         }
-        System.out.println("arr = " + Arrays.toString(arr));
+        System.out.println("\nint[] randomArray = " + Arrays.toString(randomArray));
         //find min and max with foreach loop (without Arrays.sort())
-        int min = arr[0];
-        int max = arr[0];
-        for (int i: arr) {
-            if (i < min){
+        int min = randomArray[0];
+        int max = randomArray[0];
+        for (int i : randomArray) {
+            if (i < min) {
                 min = i;
             }
-            if(i > max){
+            if (i > max) {
                 max = i;
             }
         }
         System.out.println("min = " + min);
         System.out.println("max = " + max);
-        //sort array and find sum except first and last elements
-        Arrays.sort(arr);
-        System.out.println("sorted arr = " + Arrays.toString(arr));
+
+        //sort an array and find sum except the first and the last elements
+        Arrays.sort(randomArray);
+        System.out.println("Sorted randomArray = " + Arrays.toString(randomArray));
         int sum = 0;
-        for (int i = 1; i < SIZE - 1; i++){
-            sum += arr[i];
+        for (int i = 1; i < SIZE - 1; i++) {
+            sum += randomArray[i];
         }
-        System.out.println("sum = " + sum);
+        System.out.println("Sum of all elements (except min = " + min + " and max = " + max + ") = "  + sum);
     }
 
     /*Создать строку с произвольным словом или набором букв. Преобразовать в массив.
-    Вывести массив каждую букву(символ).*/
-    public static void stringToCharArray(String str){
-        System.out.println(str);
-        char [] arr = str.toCharArray();
-        for (char w: arr) {
-            System.out.println(w);
+    Вывести массив каждую букву (символ).*/
+    public static void stringToCharArray(String str) {
+        System.out.println("\nString for transforming to char array: " + str);
+        char[] arr = str.toCharArray();
+
+        System.out.print("\nchar[] arr = ");
+        for (char i : arr) {
+            System.out.print("'" + i + "' ");
         }
     }
 }
